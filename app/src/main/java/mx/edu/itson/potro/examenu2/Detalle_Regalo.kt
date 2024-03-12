aaae00d3f6875993213e92ae67e06b3611d3d70f
Sebastian <sebassonic2341@gmail.com>
2024-03-12 16:25:57 -0700

    ExameU2 - Aplicaciones Moviles

---
+++ app/src/main/java/mx/edu/itson/potro/examenu2/Detalle_Regalo.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.potro.examenu2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Detalle_Regalo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_regalo)
+    }
+}
---
+++ app/src/main/java/mx/edu/itson/potro/examenu2/Regalo.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.potro.examenu2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Regalo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regalo)
+    }
+}
